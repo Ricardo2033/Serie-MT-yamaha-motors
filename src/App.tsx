667fedf5178ae124d06ad88d6c0bada8aee40b49
Ricardo2033 <ricardobracho2033@gmail.com>
2026-05-16 15:03:58 -0400

    app.tsx se trabajo en mejorar el rendimiento

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,146 @@ import mt10Hover from "./assets/Motos/mt 10/mt-10-hover.webp";
 import mt10HeroModal from "./assets/Motos/mt 10/yamaha-mt-10-2017-wide.webp";
 import FondoCards from "../public/carsd-f.png";
 
+  interface Moto {
+    id: number;
+    nombre: string;
+    img: string;
+    imgHover: string;
+    imgHeroModal: string;
+    imgModal?: string;
+    img1?: string;
+    titulo: string;
+    motor: string;
+    peso: string;
+    valvulas: string;
+    potencia: string;
+    torque: string;
+    tecnologias: string[];
+    descripcion?: string;
+  }
+
+const SerieMt = [
+  {
+    id: 1,
+    nombre: "MT-15",
+    img: mt15,
+    imgHover: mt15Hover,
+    imgHeroModal: mt15HeroModal,
+    img1: mt15Hover,
+    titulo: "Rendimiento Hiper-Naked a Escala",
+    motor: "Monocilíndrico de 155 cc",
+    peso: "141 kg",
+    valvulas: "4",
+    potencia: "18.4 hp a 10,000 rpm",
+    torque: "14.1 Nm a 7,500 rpm",
+    tecnologias: [
+      "Sistema VVA (Apertura Variable de Válvulas)",
+      "embrague antirrebote (Assist & Slipper)",
+      "TCS (Traction Control System)",
+    ],
+  },
+  {
+    id: 2,
+    nombre: "MT-25",
+    img: mt25,
+    imgHover: mt25Hover,
+    imgHeroModal: mt25HeroModal,
+    img1: mt25Hover,
+    titulo: "Diseñada para Escapar de la Rutina",
+    motor: "Bicilíndrico en línea, 249 cc, 4 tiempos",
+    peso: "167 kg",
+    valvulas: "8",
+    potencia: "35.5 hp a 12,000 rpm",
+    torque: "22.6 Nm a 10,000 rpm",
+    tecnologias: [
+      "Cilindros de Aluminio DiASil",
+      "Pistones Forjados de Aluminio",
+      "Iluminación LED con Predator Look",
+      "Inyección Electrónica Avanzada (TCI)",
+    ],
+  },
+  {
+    id: 3,
+    nombre: "MT-03",
+    img: mt03,
+    imgHover: mt03Hover,
+    imgHeroModal: mt03HeroModal,
+    imgModal: mt03Hover,
+    titulo: "El Equilibrio Perfecto entre Agilidad y Potencia",
+    motor: "Bicilíndrico en línea de 321 cc,",
+    peso: "168kg",
+    valvulas: "8",
+    potencia: "42.0 hp a 10,750 rpm",
+    torque: "29.5 Nm de torque a 9,000 rpm",
+    tecnologias: [
+      "Pistones de Aluminio Forjado",
+      "Cilindros DiASil",
+      "Inyección Electrónica de 12 Agujeros",
+    ],
+  },
+  {
+    id: 4,
+    nombre: "MT-07",
+    img: mt07,
+    imgHover: mt07Hover,
+    imgHeroModal: mt07HeroModal,
+    imgModal: mt07Hover,
+    img1: mt07Hover,
+    titulo: "La Máquina de Sonrisas Definitiva",
+    motor: "Bicilíndrico en línea CP2 de 689 cc",
+    peso: "183 kg",
+    valvulas: "8",
+    potencia: "73.8 hp a 9,000 rpm",
+    torque: "67 Nm de torque a 6,500 rpm",
+    tecnologias: [
+      "Acelerador Electrónico YCC-T (Yamaha Chip Controlled Throttle)",
+      "Modos de Entrega de Potencia (D-Mode)",
+    ],
+  },
+  {
+    id: 5,
+    nombre: "MT-09",
+    img: mt09,
+    imgHover: mt09Hover,
+    imgHeroModal: mt09HeroModal,
+    imgModal: mt09Hover,
+    img1: mt09Hover,
+    titulo: "El Poder Descomunal del Motor Tricilíndrico",
+    motor: "Tricilíndrico en línea CP3 de 890 cc",
+    peso: "193 kg",
+    valvulas: "12 ",
+    potencia: "119 hp a 10,000 rpm",
+    torque: "93 Nm de torque a 7,000 rpm",
+    tecnologias: [
+      "IMU de 6 Ejes (Inertial Measurement Unit)",
+      "Electrónica Predictiva sensible a la inclinación",
+      "Quickshifter Bidireccional (QSS)",
+      "Bomba de Freno Radial Brembo",
+    ],
+  },
+  {
+    id: 6,
+    nombre: "MT-10",
+    img: mt10,
+    imgHover: mt10Hover,
+    imgHeroModal: mt10HeroModal,
+    imgModal: mt10Hover,
+    img1: mt10Hover,
+    titulo: "El Rey de las Master of Torque",
+    motor: "Tetracilíndrico en línea CP4 de 998 cc",
+    peso: "214 kg",
+    valvulas: "16 ",
+    potencia: "165.9 hp a 11,500 rpm",
+    torque: "112 Nm de torque a 9,000 rpm",
+    tecnologias: [
+      "Cigüeñal Crossplane CP4",
+      "Bielas de Acero Fracturadas por Cementación",
+      "Módulo Electrónico Avanzado (YRC)",
+      "Pantalla TFT a color con Conectividad",
+    ],
+  },
+];
+
 function App() {
   const [tarjetaVisible, setTarjetaVisible] = useState(false);
 
@@ -37,131 +177,9 @@ function App() {
     setTarjetaVisible(false);
   };
 
-  const SerieMt = [
-    {
-      id: 1,
-      nombre: "MT-15",
-      img: mt15,
-      imgHover: mt15Hover,
-      imgHeroModal: mt15HeroModal,
-      img1: mt15Hover,
-      titulo: "Rendimiento Hiper-Naked a Escala",
-      motor: "Monocilíndrico de 155 cc",
-      peso: "141 kg",
-      valvulas: "4",
-      potencia: "18.4 hp a 10,000 rpm",
-      torque: "14.1 Nm a 7,500 rpm",
-      tecnologias: [
-        "Sistema VVA (Apertura Variable de Válvulas)",
-        "embrague antirrebote (Assist & Slipper)",
-        "TCS (Traction Control System)",
-      ],
-    },
-    {
-      id: 2,
-      nombre: "MT-25",
-      img: mt25,
-      imgHover: mt25Hover,
-      imgHeroModal: mt25HeroModal,
-      img1: mt25Hover,
-      titulo: "Diseñada para Escapar de la Rutina",
-      motor: "Bicilíndrico en línea, 249 cc, 4 tiempos",
-      peso: "167 kg",
-      valvulas: "8",
-      potencia: "35.5 hp a 12,000 rpm",
-      torque: "22.6 Nm a 10,000 rpm",
-      tecnologias: [
-        "Cilindros de Aluminio DiASil",
-        "Pistones Forjados de Aluminio",
-        "Iluminación LED con Predator Look",
-        "Inyección Electrónica Avanzada (TCI)",
-      ],
-    },
-    {
-      id: 3,
-      nombre: "MT-03",
-      img: mt03,
-      imgHover: mt03Hover,
-      imgHeroModal: mt03HeroModal,
-      imgModal: mt03Hover,
-      titulo: "El Equilibrio Perfecto entre Agilidad y Potencia",
-      motor: "Bicilíndrico en línea de 321 cc,",
-      peso: "168kg",
-      valvulas: "8",
-      potencia: "42.0 hp a 10,750 rpm",
-      torque: "29.5 Nm de torque a 9,000 rpm",
-      tecnologias: [
-        "Pistones de Aluminio Forjado",
-        "Cilindros DiASil",
-        "Inyección Electrónica de 12 Agujeros",
-      ],
-    },
-    {
-      id: 4,
-      nombre: "MT-07",
-      img: mt07,
-      imgHover: mt07Hover,
-      imgHeroModal: mt07HeroModal,
-      imgModal: mt07Hover,
-      img1: mt07Hover,
-      titulo: "La Máquina de Sonrisas Definitiva",
-      motor: "Bicilíndrico en línea CP2 de 689 cc",
-      peso: "183 kg",
-      valvulas: "8",
-      potencia: "73.8 hp a 9,000 rpm",
-      torque: "67 Nm de torque a 6,500 rpm",
-      tecnologias: [
-        "Acelerador Electrónico YCC-T (Yamaha Chip Controlled Throttle)",
-        "Modos de Entrega de Potencia (D-Mode)",
-      ],
-    },
-    {
-      id: 5,
-      nombre: "MT-09",
-      img: mt09,
-      imgHover: mt09Hover,
-      imgHeroModal: mt09HeroModal,
-      imgModal: mt09Hover,
-      img1: mt09Hover,
-      titulo: "El Poder Descomunal del Motor Tricilíndrico",
-      motor: "Tricilíndrico en línea CP3 de 890 cc",
-      peso: "193 kg",
-      valvulas: "12 ",
-      potencia: "119 hp a 10,000 rpm",
-      torque: "93 Nm de torque a 7,000 rpm",
-      tecnologias: [
-        "IMU de 6 Ejes (Inertial Measurement Unit)",
-        "Electrónica Predictiva sensible a la inclinación",
-        "Quickshifter Bidireccional (QSS)",
-        "Bomba de Freno Radial Brembo",
-      ],
-    },
-    {
-      id: 6,
-      nombre: "MT-10",
-      img: mt10,
-      imgHover: mt10Hover,
-      imgHeroModal: mt10HeroModal,
-      imgModal: mt10Hover,
-      img1: mt10Hover,
-      titulo: "El Rey de las Master of Torque",
-      motor: "Tetracilíndrico en línea CP4 de 998 cc",
-      peso: "214 kg",
-      valvulas: "16 ",
-      potencia: "165.9 hp a 11,500 rpm",
-      torque: "112 Nm de torque a 9,000 rpm",
-      tecnologias: [
-        "Cigüeñal Crossplane CP4",
-        "Bielas de Acero Fracturadas por Cementación",
-        "Módulo Electrónico Avanzado (YRC)",
-        "Pantalla TFT a color con Conectividad",
-      ],
-    },
-  ];
-
   const currentBg = tarjetaVisible ? FondoCards : hero;
 
-  const [selecMoto, setSelecMoto] = useState<any | null>(null);
+  const [selecMoto, setSelecMoto] = useState<Moto | null>(null);
 
   return (
     <div className="relative min-h-screen w-full bg-black flex flex-col overflow-x-hidden">
@@ -256,7 +274,7 @@ function App() {
                   {/* Título Principal */}
                   <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-[Rajdhani] font-black text-white italic uppercase leading-[0.95] tracking-tighter mb-4 drop-shadow-[0_4px_12px_rgba(0,0,0,0.5)] wrap-break-words">
                     {selecMoto.nombre} <br />
-                    <span className="text-zinc-500/40 text-xl sm:text-3xl md:text-5xl lg:text-6xl block mt-2 leading-tight"> 
+                    <span className="text-zinc-500/40 text-xl sm:text-3xl md:text-5xl lg:text-6xl block mt-2 leading-tight">
                       {selecMoto.titulo}
                     </span>
                   </h1>
